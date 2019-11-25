35bd0086a5cc6e37743516140242c775e71a79d6
Boaph <Boaph5Du@rambler.ru>
2019-11-25 18:22:23 +0300

    Merge conflict

--- src/App.js
+++ src/App.js
@@ -2,23 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './normalize.css';
 import './style.css';
-<<<<<<< HEAD
-import Header from './ui/elements/Header';
-=======
-import WelcomeScreen from './ui/screens/WelcomeScreen';
 import SendScreen from './ui/screens/SendScreen';
->>>>>>> 8d95d9f94bcdcfcf630b71d273195960fc748ace
 
 class App extends React.Component {
   render() {
     return (
-<<<<<<< HEAD
-      <div class="container container-p">
-          <Header />
-        </div>
-=======
         <SendScreen/>
->>>>>>> 8d95d9f94bcdcfcf630b71d273195960fc748ace
     );
   }
 }
