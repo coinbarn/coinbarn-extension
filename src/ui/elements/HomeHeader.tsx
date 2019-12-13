3b163726cf308b5d6f1369c1337747416dbc363e
catena <catena@protonmail.com>
2019-12-13 17:45:29 +0300

    closeBtn in header

--- src/ui/elements/HomeHeader.tsx
+++ src/ui/elements/HomeHeader.tsx
@@ -3,15 +3,20 @@ import React from 'react';
 interface IHomeHeaderProps {
   toggleSettings: () => void
   toggleMenuBlock: () => void
+  settingsOn: boolean
+  aboutOn: boolean
 }
 
 export default class HomeHeader extends React.Component<IHomeHeaderProps, {}> {
 
   public render() {
+    let menuBtn = this.props.aboutOn ? 'closeBtn' : 'menuBtn';
+    let settingsBtn = this.props.settingsOn ? 'closeBtn' : 'settingsBtn';
+
     return (
       <div className='homeHeader'>
-        <button className='menuBtn' onClick={this.props.toggleMenuBlock}/>
-        <button className='settingsBtn' onClick={this.props.toggleSettings}/>
+        <button className={menuBtn} onClick={this.props.toggleMenuBlock}/>
+        <button className={settingsBtn} onClick={this.props.toggleSettings}/>
       </div>
     );
   }
