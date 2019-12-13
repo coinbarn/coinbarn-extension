b90bfc80b1152607477da20fe1e3d2df68f1c6cb
catena <catena@protonmail.com>
2019-12-13 16:14:39 +0300

    About window buttons

--- src/ui/AboutWindow.tsx
+++ src/ui/AboutWindow.tsx
@@ -2,33 +2,33 @@ import React from 'react';
 import Constants from "../Constants";
 
 interface IAboutWindowProps {
-	toggle: (a: any) => void
+  toggle: (a: any) => void
 }
 
 
 export default class AboutWindow extends React.Component<IAboutWindowProps> {
   public render() {
     return (
-			<div className='menu'>
-    		<button className='backBtnWhite' onClick={this.props.toggle}></button>
-    		<div className='greeting'>About</div>
-    		<h1>CoinBarn Version</h1>
-				{/*TODO: get version automatically*/}
-    		<h1>0.1.0</h1>
-    		<h1 className='documents'>Documents</h1>
-    		<ul>
-    		  <li><h3> <a target="_blank" rel="noopener noreferrer" href={Constants.privacyURL}>Privacy policy</a></h3> </li>
-    		  <li><h3> <a target="_blank" rel="noopener noreferrer" href={Constants.termsURL}>Terms of use</a>  </h3> </li>
-    		  {/*<li><h3> <a href='#'>Attributions</a>  </h3> </li>*/}
-    		</ul>
-    		<div className='footer'>
-    		  <ul>
-    		    <li><h3><a href='#'>Support</a></h3></li>
-    		    <li><h3><a href='#'>Site</a></h3></li>
-    		    <li><h3><a href='#'>Email us!</a></h3></li>
-    		  </ul>
-    		</div>
-			</div>
+      <div className='menu'>
+        <button className='backBtnWhite' onClick={this.props.toggle}></button>
+        <div className='greeting'>About</div>
+        <h1>CoinBarn Version</h1>
+        {/*TODO: get version automatically*/}
+        <h1>0.1.0</h1>
+        <h1 className='documents'>Documents</h1>
+        <ul>
+          <li><h3><a target="_blank" rel="noopener noreferrer" href={Constants.privacyURL}>Privacy policy</a></h3></li>
+          <li><h3><a target="_blank" rel="noopener noreferrer" href={Constants.termsURL}>Terms of use</a></h3></li>
+          {/*<li><h3> <a href='#'>Attributions</a>  </h3> </li>*/}
+        </ul>
+        <div className='footer'>
+          <ul>
+            <li><h3><a href="mailto:coinbarn@protonmail.com?subject=coinbarn support">Support</a></h3></li>
+            <li><h3><a target="_blank" rel="noopener noreferrer" href={Constants.siteURL}>Site</a></h3></li>
+            <li><h3><a href="mailto:coinbarn@protonmail.com?subject=coinbarn idea">Email us!</a></h3></li>
+          </ul>
+        </div>
+      </div>
     );
   }
 }
