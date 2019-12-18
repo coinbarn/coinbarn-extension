31be4d6bf78b798131da2d8cf8027af4c00083ec
Boaph <Boaph5Du@rambler.ru>
2019-12-18 13:34:18 +0300

    Extended logout time

--- src/Constants.ts
+++ src/Constants.ts
@@ -8,7 +8,7 @@ export default class Constants {
   public static privacyURL: string =
     "https://github.com/coinbarn/coinbarn-extension/blob/master/PRIVACY.md";
   // Logout time in milliseconds
-  public static autoLogoutTime: number = 10000;
+  public static autoLogoutTime: number = 120000;
   public static minTokenNameLength: number = 2;
   public static maxTokenNameLength: number = 8;
   public static secretPath = "m/44'/429'/0'/0/0";
